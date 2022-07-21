0d77f21dec1254cfabfd5504fb340a0c247f55fe
zhou <zhoutjcc@msn.cn>
2022-07-21 20:25:45 +0800

    add file

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,11 +1,11 @@
 dataset_paths = {
-	'celeba_train': '',
-	'celeba_test': '',
+	'celeba_train': './data_train',
+	'celeba_test': './data_test',
 	'celeba_train_sketch': '',
 	'celeba_test_sketch': '',
 	'celeba_train_segmentation': '',
 	'celeba_test_segmentation': '',
-	'ffhq': '',
+	'ffhq': '/home/zhou/Documents/python/DualStyleGAN/data/head2/images/train',
 }
 
 model_paths = {
--- scripts/train.py
+++ scripts/train.py
@@ -11,22 +11,36 @@ sys.path.append("..")
 
 from options.train_options import TrainOptions
 from training.coach import Coach
-
+import shutil
 
 def main():
-	opts = TrainOptions().parse()
-	if os.path.exists(opts.exp_dir):
-		raise Exception('Oops... {} already exists'.format(opts.exp_dir))
-	os.makedirs(opts.exp_dir)
+    opts = TrainOptions().parse()
+    opts.dataset_type = "ffhq_encode"
+    opts.exp_dir = "./output"
+    opts.workers = 8
+    opts.atch_size = 8
+    opts.test_batch_size = 8
+    opts.test_workers = 8
+    opts.val_interval = 2500
+    opts.save_interval = 5000
+    opts.encoder_type = "MobileGradualStyleEncoder"
+    opts.start_from_latent_avg = True
+    opts.lpips_lambda = 0.8
+    opts.l2_lambda = 1
+    opts.id_lambda = 0.1
+    if os.path.exists(opts.exp_dir):
+        shutil.rmtree(opts.exp_dir)
 
-	opts_dict = vars(opts)
-	pprint.pprint(opts_dict)
-	with open(os.path.join(opts.exp_dir, 'opt.json'), 'w') as f:
-		json.dump(opts_dict, f, indent=4, sort_keys=True)
+    os.makedirs(opts.exp_dir)
 
-	coach = Coach(opts)
-	coach.train()
+    opts_dict = vars(opts)
+    pprint.pprint(opts_dict)
+    with open(os.path.join(opts.exp_dir, 'opt.json'), 'w') as f:
+        json.dump(opts_dict, f, indent=4, sort_keys=True)
+
+    coach = Coach(opts)
+    coach.train()
 
 
 if __name__ == '__main__':
-	main()
+    main()
