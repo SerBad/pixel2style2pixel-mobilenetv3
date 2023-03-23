7b8a08b76ad3420af845f559be30c4ba355d02e5
zhou <zhoutjcc@msn.cn>
2023-03-23 22:13:56 +0800

    add kaggle file

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -6,8 +6,8 @@ DATASETS = {
 	'ffhq_encode': {
 		'transforms': transforms_config.EncodeTransforms,
 		'train_source_root': dataset_paths['ffhq'],
-		'train_target_root': dataset_paths['celeba_train'],
-		'test_source_root': dataset_paths['ffhq'],
+		'train_target_root': dataset_paths['ffhq'],
+		'test_source_root': dataset_paths['celeba_test'],
 		'test_target_root': dataset_paths['celeba_test'],
 	},
 	'ffhq_frontalize': {
