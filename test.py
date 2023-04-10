bb0c186b0e54145ae3a2392db295e0aea8392607
zhou <zhoutjcc@msn.cn>
2023-04-10 18:03:45 +0800

    add kaggle file

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from PIL import Image
 
+# from_im = Image.open("/home/zhou/Downloads/黄种人-FFHQ/63323.png")
+from_im = Image.open("/kaggle/input/zhoudualstylegan/DualStyleGAN/data/yellow-FFHQ/33677.png")
 from_im = Image.open("/kaggle/input/zhoudualstylegan/DualStyleGAN/data/yellow-FFHQ/63323.png")
 from_im.show()
