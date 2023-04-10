289ff16eb7067efcfab2910035aeed9753631bd1
zhou <zhoutjcc@msn.cn>
2023-04-10 20:55:23 +0800

    add kaggle file

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 # print(os.path.getsize("s.png"))
 
 # from_im = Image.open("/home/zhou/Downloads/黄种人-FFHQ/55057.png")
-from_im = Image.open("/kaggle/input/zhoudualstylegan/DualStyleGAN/data/yellow-FFHQ/55057.png")
+from_im = Image.open("/kaggle/input/zhoudualstylegan/DualStyleGAN/data/yellow-FFHQ/46546.png")
 print(from_im.getbands(), len(from_im.getbands()))
 from_im = from_im.convert('RGB')
 from_im.show()
